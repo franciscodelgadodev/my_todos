131f3c0712f264c04ce52fd8c78a8d5c7538d31c
francisco delgado <francisco.delgado@agilekoding.com>
2013-08-29 22:02:59 -0500

    attrBinding useb to add completed class in index.html

--- js/fixtures/todo.js
+++ js/fixtures/todo.js
@@ -2,16 +2,16 @@ Todos.Todo.FIXTURES = [
   {
     id: 1,
     title: 'Learn Rails',
-    isCompleted: 'true'
+    isCompleted: true
   },
   {
     id: 2,
     title: 'Learn Ember.js',
-    isCompleted: 'false'
+    isCompleted: false
   },
   {
     id: 3,
     title: 'Profit!',
-    isCompleted: 'false'
+    isCompleted: false
   }
 ];
