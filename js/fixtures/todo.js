be4d79ff603822c8c491d0ff81258dfc27de8c34
francisco delgado <francisco.delgado@agilekoding.com>
2013-08-29 21:44:14 -0500

    Todos.Todo fixture data were added

---
+++ js/fixtures/todo.js
@@ -0,0 +1,17 @@
+Todos.Todo.FIXTURES = [
+  {
+    id: 1,
+    title: 'Learn Rails',
+    isCompleted: 'true'
+  },
+  {
+    id: 2,
+    title: 'Learn Ember.js',
+    isCompleted: 'false'
+  },
+  {
+    id: 3,
+    title: 'Profit!',
+    isCompleted: 'false'
+  }
+];
