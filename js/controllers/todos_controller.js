3af9e43be3cc7a50174aed622f085f33a699d48a
francisco delgado <francisco.delgado@agilekoding.com>
2013-08-31 23:55:41 -0500

    counter for items left added

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -20,5 +20,14 @@ Todos.TodosController = Ember.ArrayController.extend({
       title: this.get('newTitle'),
       isCompleted: false
     });
-  }
+  },
+
+	remaining: function() {
+		return this.filterProperty('isCompleted', false).get('length');
+	}.property('@each.isCompleted'),
+
+	inflection: function() {
+		var remaining = this.get('remaining');
+		return remaining === 1 ? 'item' : 'items';
+	}.property('remaining')
 });
